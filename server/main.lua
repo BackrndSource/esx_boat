970d6e992e076b521e2bce010d49b9d083be2319
BackrndSource <back.rndsource@gmail.com>
2019-03-27 15:22:41 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -29,7 +29,7 @@ ESX.RegisterServerCallback('esx_boat:buyBoat', function(source, cb, vehicleProps
 	if xPlayer.getMoney() >= price then
 		xPlayer.removeMoney(price)
 
-		MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle, type) VALUES (@owner, @plate, @vehicle, @type)', {
+		MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle, type, stored) VALUES (@owner, @plate, @vehicle, @type, @stored)', {
 			['@owner']   = xPlayer.identifier,
 			['@plate']   = vehicleProps.plate,
 			['@vehicle'] = json.encode(vehicleProps),
