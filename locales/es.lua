3a9259c86a84f9cd7b98273da76f9f9aac5ed633
BackrndSource <back.rndsource@gmail.com>
2019-03-27 15:15:59 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,31 @@
+Locales['es'] = {
+  -- shop
+  ['boat_shop'] = 'tienda de embarcaciones',
+  ['boat_shop_open'] = 'pulsa ~INPUT_CONTEXT~ para comprar en la ~y~tienda de embarcaciones~s~.',
+  ['boat_shop_confirm'] = '¿Comprar <span style="color: yellow;">%s</span> por <span style="color: orange;">%s€</span>?',
+  ['boat_shop_bought'] = 'has ~y~comprado~s~ un ~b~%s~s~ por ~g~%s€~s~',
+  ['boat_shop_nomoney'] = 'no tienes suficiente dinero...',
+  ['confirm_no'] = 'no',
+  ['confirm_yes'] = 'si',
+
+  -- garage
+  ['garage'] = 'embarcadero',
+  ['garage_open'] = 'pulsa ~INPUT_CONTEXT~ para acceder a tu ~y~embarcadero~s~.',
+  ['garage_store'] = 'pulsa ~INPUT_CONTEXT~ para ~y~amarrar~s~ tu embarcación en el embarcadero.',
+  ['garage_taken'] = 'has desamarrado tu embarcación!',
+  ['garage_stored'] = 'tu embarcación fue amarrada sin problemas marinero!',
+  ['garage_noboats'] = 'no tienes embarcaciones! Visita la ~y~tienda de embarcaciones~s~ para comprar una.',
+  ['garage_blocked'] = 'no puedes desamarrar tu embarcación porque otra embarcación está bloqueando la salida!',
+  ['garage_notowner'] = 'esta embarcación no es tuya, pirata!',
+
+  -- license
+  ['license_menu'] = 'Licencia de Navegación',
+  ['license_buy_no'] = 'no',
+  ['license_buy_yes'] = '¿Quieres comprar la Licencia de Navegación <span style="color: green;">%s€</span>?',
+  ['license_bought'] = 'has comprado la ~y~Licencia de Navegación~s~ por ~g~%s€~s~',
+  ['license_nomoney'] = 'no tienes suficiente dinero para la ~y~Licencia de Navegación~s~...',
+
+  -- blips
+  ['blip_garage'] = 'embarcadero',
+  ['blip_shop'] = 'tienda de embarcaciones',
+}
